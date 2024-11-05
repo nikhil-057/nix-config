a8a7948423dfd34dceb83e6f689ff6e15daf9571
nikhil <asdfasdf5790@gmail.com>
2024-11-05 09:00:10 +0000

    using hm for dotfile management

--- home-manager/setup.sh
+++ home-manager/setup.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cd "$(dirname "$0")"
-
-rm -rf ~/.config/home-manager && ln -rsfv . ~/.config/home-manager
-
-## TODO: move these into home.nix
-#mkdir -p ~/.config/nvim
-ln -rsfv dotfiles/init.lua ~/.config/nvim/init.lua
-ln -rsfv dotfiles/tmux.conf ~/.tmux.conf
-ln -rsfv dotfiles/profile ~/.profile
-ln -rsfv dotfiles/zshrc ~/.zshrc
+rm -rf ~/.config/home-manager
+ln -rsfv . ~/.config/home-manager
+home-manager switch
