35bbbaba0636d9d42943e42ea66a533f3fdc13c2
nikhil <asdfasdf5790@gmail.com>
2024-11-13 18:39:21 +0000

    minor update

--- home-manager/setup.sh
+++ home-manager/setup.sh
@@ -6,7 +6,7 @@ rm -rf ~/.config/home-manager
 ln -rsfv . ~/.config/home-manager
 home-manager switch
 
-## need the following to circumvent issues around podman and seduid
+## need the following because podman does not play nicely with newuidmap
 ## more reading: https://github.com/nix-community/home-manager/pull/4801#issuecomment-2308715379
 if [ -n "$(command -v podman)" ] && [ -z "$(command -v newuidmap)" ] && [ -f "/etc/debian_version" ]
 then
