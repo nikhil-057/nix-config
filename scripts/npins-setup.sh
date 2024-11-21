feeafef1891a7eccab40896a1eb824bdced87341
nikhil <asdfasdf5790@gmail.com>
2024-11-21 13:39:38 +0000

    Rename

--- scripts/npins-setup.sh
+++ scripts/npins-setup.sh
@@ -5,6 +5,6 @@ trap "cd $CWD" EXIT
 cd "$(dirname "$0")/.."
 nix-shell -p npins --run " \
   npins init --bare; \
-  npins add github nixos nixpkgs --branch nixos-unstable; \
   npins add github nix-community home-manager --branch master; \
+  npins add github nixos nixpkgs --branch nixos-unstable; \
 "
