523209d5a6065fc897625878a1775fad37a40c23
nikhil <asdfasdf5790@gmail.com>
2024-11-30 19:52:25 +0000

    simplify bootstrapping

--- scripts/npins-setup.sh
+++ scripts/npins-setup.sh
@@ -3,8 +3,9 @@ set -euo pipefail
 CWD="$(pwd)"
 trap "cd $CWD" EXIT
 cd "$(dirname "$0")/.."
+export NIX_PATH="$(NIX_BUILD_SHELL=bash nix-shell --pure -A echoNixPath)"
 nix-shell -p npins --run " \
   npins init --bare; \
   npins add github nix-community home-manager --branch master; \
-  npins add github nixos nixpkgs --branch nixos-unstable; \
+  npins add github nixos nixpkgs --branch master; \
 "
--- shell.nix
+++ shell.nix
@@ -2,29 +2,35 @@
   system ? builtins.currentSystem,
 }:
 let
-configDir =
-  builtins.substring
-    0
-    (builtins.sub (builtins.stringLength __curPos.file) 10)
-    __curPos.file;
-sources = import (configDir + "/npins");
-pkgs = import sources.nixpkgs {
-  inherit system;
-  config = {};
-  overlays = [];
-};
-in
-with pkgs; stdenv.mkDerivation {
- name = "nix-user-config";
-  inherit configDir;
+  configDir =
+    builtins.substring
+      0
+      (builtins.sub (builtins.stringLength __curPos.file) 10)
+      __curPos.file;
+  sources = import (configDir + "/npins");
   nixPath =
     ":nixpkgs=" + sources.nixpkgs +
     ":home-manager=" + sources.home-manager;
-  buildInputs = [
-    nix
-  ];
-  shellHook =
-    "export NIX_PATH=\$nixPath;" +
-    "\$configDir/scripts/hm-setup.sh;" +
-    "exit";
+  pkgs = import sources.nixpkgs {
+    inherit system;
+    config = {};
+    overlays = [];
+  };
+in with pkgs; {
+  echoNixPath = stdenv.mkDerivation {
+    name = "echo-nix-path";
+    shellHook =
+      "echo ${nixPath};" +
+      "exit";
+  };
+  hmSetup = stdenv.mkDerivation {
+    name = "hm-setup";
+    buildInputs = [
+      nix
+    ];
+    shellHook =
+      "export NIX_PATH=${nixPath};" +
+      "${configDir}/scripts/hm-setup.sh;" +
+      "exit";
+  };
 }
