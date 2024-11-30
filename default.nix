fbf72c5bbf14f1202d406248cceb884a7fea4821
nikhil <asdfasdf5790@gmail.com>
2024-11-30 16:51:41 +0000

    simplify bootstrapping

---
+++ default.nix
@@ -0,0 +1,30 @@
+{
+  system ? builtins.currentSystem,
+}:
+let
+configDir =
+  builtins.substring
+    0
+    (builtins.sub (builtins.stringLength __curPos.file) 12)
+    __curPos.file;
+sources = import (configDir + "/npins");
+pkgs = import sources.nixpkgs {
+  inherit system;
+  config = {};
+  overlays = [];
+};
+in
+with pkgs; stdenv.mkDerivation {
+ name = "nix-user-config";
+  inherit configDir;
+  nixPath =
+    ":nixpkgs=" + sources.nixpkgs +
+    ":home-manager=" + sources.home-manager;
+  buildInputs = [
+    nix
+  ];
+  shellHook =
+    "export NIX_PATH=\$nixPath;" +
+    "\$configDir/scripts/hm-setup.sh;" +
+    "exit";
+}
