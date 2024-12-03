1e6dd06e89643251db78414739c3d93a20628ada
nikhil <asdfasdf5790@gmail.com>
2024-12-03 20:45:34 +0000

    improve shell hook

--- scripts/hm-shell-hook.sh
+++ scripts/hm-shell-hook.sh
@@ -6,12 +6,8 @@ cd "$(dirname "$0")/.."
 unset NIX_BUILD_SHELL
 mkdir -p ~/.config
 rm -rf ~/.config/home-manager
+nix-shell "<home-manager>" -A install
+rm -rf ~/.config/home-manager
 ln -rsfv home-manager ~/.config/home-manager
 rm -f ~/.gitconfig
-nix-shell "<home-manager>" -A install
-## need the following because podman does not play nicely with newuidmap
-## more reading: https://github.com/nix-community/home-manager/pull/4801#issuecomment-2308715379
-if [ -n "$(command -v podman)" ] && [ -z "$(command -v newuidmap)" ] && [ -f "/etc/debian_version" ]
-then
-    sudo apt install uidmap --yes
-fi
+~/.nix-profile/bin/home-manager switch -b backup
