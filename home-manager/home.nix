e6c7acefd6aa363b5a9ae3512b545dd414e5c767
nikhil <asdfasdf5790@gmail.com>
2024-12-12 00:25:36 +0530

    minor updates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }: {
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # Home Manager needs a bit of information about you and the paths it should manage.
   home.username = "nikhil";
   home.homeDirectory = "/home/nikhil";
 
@@ -16,8 +15,18 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
+  #
+  # It is sometimes useful to fine-tune packages, for example, by applying
+  # overrides. You can do that directly here, just don't forget the
+  # parentheses. Maybe you want to install Nerd Fonts with a limited number of fonts?
+  #   (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  #
+  # You can also create simple shell scripts directly inside your
+  # configuration. For example, this adds a command 'my-hello' to your environment:
+  #   (pkgs.writeShellScriptBin "my-hello" ''
+  #     echo "Hello, ${config.home.username}!"
+  #   '')
   home.packages = [
-
     pkgs.hello
     pkgs.git
     pkgs.neovim
@@ -32,35 +41,20 @@
     pkgs.ripgrep
     pkgs.fd
     pkgs.docker
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
+  # Building this configuration will create a copy of 'dotfiles/<filename>' in
+  # the Nix store. Activating the configuration will then make '~/<filename>' a
+  # symlink to the Nix store copy.
+  #
+  # You can also set the file content immediately.
+  #   ".gradle/gradle.properties".text = ''
+  #     org.gradle.console=verbose
+  #     org.gradle.daemon.idletimeout=3600000
+  #   '';
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
     ".config/nvim/init.lua".source = dotfiles/init.lua;
     ".tmux.conf".source = dotfiles/tmux.conf;
     ".profile".source = dotfiles/profile;
