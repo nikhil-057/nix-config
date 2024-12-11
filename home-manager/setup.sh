284681f63def1b221db11741e0224e3599a17813
nikhil <asdfasdf5790@gmail.com>
2024-12-12 00:25:36 +0530

    switch from docker to podman

--- home-manager/setup.sh
+++ home-manager/setup.sh
@@ -5,3 +5,9 @@ mkdir -p ~/.config
 rm -rf ~/.config/home-manager
 ln -rsfv . ~/.config/home-manager
 home-manager switch
+
+## https://github.com/nix-community/home-manager/pull/4801#issuecomment-2308715379
+if [ -n "$(command -v podman)" ] && [ -z "$(command -v newuidmap)" ]
+then
+    sudo apt install uidmap --yes
+fi
