7045f52214435adca5be61d339f8fef48f286758
nikhil <asdfasdf5790@gmail.com>
2025-02-05 22:55:48 +0530

    add nixvim

--- setup/npins.sh
+++ setup/npins.sh
@@ -8,4 +8,5 @@ nix-shell -p npins --run " \
   npins init --bare; \
   npins add github nixos nixpkgs --branch nixos-unstable; \
   npins add github nikhil-057 home-manager --branch customizable-shellhook; \
+  npins add github nix-community nixvim --branch main; \
 "
